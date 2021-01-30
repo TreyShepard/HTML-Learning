92ff749edd3a5d0920844f79d087ec1416628a01
TreyShepard <ljshepard97@gmail.com>
2021-01-29 20:24:20 -0500

    changed index to a php

---
+++ Resume1.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Trey Shepard | Resume</title>
+<!--CSS Reference-->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<div class="container-fluid">
+  <a class="navbar-brand" href="Resume1.html">Trey Shepard</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse navbar-toggler" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="Page 1.html"><span class="sr-only">Current Project</span></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<br>
+</div>
+<!--Panels-->
+<!--Education-->
+<div class="container">
+<div class="card">
+  <div class="card-header"><strong>
+    EDUCATION</strong>  
+  </div>
+  <div class="card-body">
+		<div class="row">
+    		<div class="col-2" style="font-size: 12px"><center><img src="Images/UMBC Short.png" width="80px"></center><br><center><strong>Graduation Date: 05-2021</strong></center></div>
+    		<div class="col-3" align="center" style="font-size:15px"><br><br><strong>
+    			University of Maryland, Baltimore County<br>
+    			Financial Economics (B.S)</strong><br><br>
+    		</div>
+    		<div class="col-7" align="center" style="font-size:15px"><br><br>
+    			<em><u>Relevant Coursework:</u>&nbsp;Economic Forecasting, Econometrics, Analytical Methods in Economics, International Finance, Intermediate Accounting I & II, Cost Accounting, Financial Management, Applied Statistics for Business and Economics</em> 
+    		</div>
+  		</div><br>
+  			<!--Block for a completed school section-->
+  		<div class="row">
+    		<div class="col-2" style="font-size: 12px"><center><img src="Images/WSCC.jpg" width="80px"></center><br><center><strong>Fall 2016- Spring 2018</strong></center></div>
+    		<div class="col-3" align="center" style="font-size: 15px"><br><strong>
+    			Walters State Community College<br>
+    			Associates Degree</strong><br><br>
+    		</div>
+    		<div class="col-7">
+    			<em></em> 
+    		</div>
+  		</div>
+  </div>
+</div>
+</div>
+<br>
+
+<!--Skills-->
+<div class="container">
+<div class="card">
+  <div class="card-header"><strong>
+    SKILLS </strong>  
+  </div>
+  <div class="card-body">
+  		<div class="row">
+      		<div class="col-3"><strong>HTML</strong></div>
+      		<div class="col-3"><strong>Excel</strong></div>
+      		<div class="col-3"><strong>PowerPoint</strong></div>
+      		<div class="col-3"><strong>STATA</strong></div>
+      	</div>
+      	<br>
+      <footer class="footer"><i><sub> Updated on: 01/29/2021</sub></i></footer>
+  </div>
+</div>
+</div>
+<br>
+<!--Experience-->
+<div class="container">
+<div class="card">
+  <div class="card-header"><strong>
+    EXPERIENCE </strong>  
+  </div>
+  <div class="card-body">
+  		<u><strong>Intern | Corporate Trainer: </strong>Centric Business Systems, Owings Mills, MD</u><br>
+  		Deployed training and consultive servicing with management and sales teams to increase customer satisfaction and drive additional revenue.<br>
+  		<ul>
+  			<li>Assisted management to train new employees on products and services.</li>
+  			<li>Scheduled and gave trainings to clients to help them better understand the functionality of their equipment and software.</li>
+  			<li>Worked with sales representatives to create additional value and solve issues for potential clients.</li>
+  			<li>Autonomously maintained a full schedule to maximize work efficiency, maximize assistance resources to various internal groups, and improve availability of informational resources for clients.</li>
+  		</ul>
+      <footer class="footer"><i><sub>June 2019 - August 2019</sub></i></footer>
+  </div>
+</div>
+</div>
+</div>
+<br><br>
+<p>&copy; Trey Shepard - All Rights Reserved</p>
+<!--JS Reference-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</body>
+</html>
